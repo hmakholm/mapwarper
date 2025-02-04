5075e2d206022b90b8c46db43ba5dc56144c75a8
Henning Makholm <henning@makholm.net>
2025-02-04 22:17:03 +0100

    Lens rectangle can be resized dynamically

--- src/net/makholm/henning/mapwarper/gui/LensTool.java
+++ src/net/makholm/henning/mapwarper/gui/LensTool.java
@@ -1,5 +1,8 @@
 package net.makholm.henning.mapwarper.gui;
 
+import java.awt.Cursor;
+import java.util.function.Function;
+
 import net.makholm.henning.mapwarper.geometry.AxisRect;
 import net.makholm.henning.mapwarper.geometry.Point;
 import net.makholm.henning.mapwarper.gui.overlays.BoxOverlay;
@@ -13,14 +16,22 @@ public class LensTool extends Tool {
 
   @Override
   public ToolResponse mouseResponse(Point pos, int modifiers) {
-    BoxOverlay currentLens = mapView().lensRect;
-    if( currentLens == null )
-      return NO_RESPONSE;
-    else if( currentLens.box.contains(pos) ) {
+    var dragAction = startDrag(pos);
+    if( dragAction != null ) {
+      return new ToolResponse() {
+        @Override
+        public Cursor cursor() {
+          return dragAction.cursor();
+        }
+        @Override
+        public void execute(ExecuteWhy why) {
+          if( dragAction.cursor() == null )
+            mapView().cancelLens();
+        }
+      };
+    } else {
       var r = ctrlHeld(modifiers) ? lensMinusCommand() : lensPlusCommand();
       return r == null ? NO_RESPONSE : why -> r.run();
-    } else {
-      return why -> mapView().cancelLens();
     }
   }
 
@@ -66,9 +77,11 @@ public class LensTool extends Tool {
 
   @Override
   public MouseAction drag(Point pos0, int modifiers0) {
+    var dragAction0 = startDrag(pos0);
     mapView().cancelLens();
+    var dragAction = dragAction0 != null ? dragAction0 : startDrag(pos0);
     return (pos1, modifiers1) -> {
-      AxisRect chosen = new AxisRect(pos0, pos1);
+      AxisRect chosen = dragAction.makeRect.apply(pos1);
       var overlay = createBox(chosen);
       return new ToolResponse() {
         @Override
@@ -76,11 +89,69 @@ public class LensTool extends Tool {
           return overlay;
         }
         @Override
+        public Cursor cursor() {
+          return dragAction.cursor();
+        }
+        @Override
         public void execute(ExecuteWhy why) {
           mapView().setLens(overlay);
         }
       };
     };
+  }
+
+  private record DragAction(Cursor cursor, Function<Point, AxisRect> makeRect) {}
+
+  private DragAction startDrag(Point p1) {
+    if( mapView().lensRect != null ) {
+      var box = mapView().lensRect.box;
+      int code = 10*placecode(p1.x, box.xmin(), box.xmax()) +
+          placecode(p1.y, box.ymin(), box.ymax());
+      switch( code ) {
+      case 11:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.NW_RESIZE_CURSOR),
+            p2 -> new AxisRect(p2, Point.at(box.xmax(), box.ymax())));
+      case 31:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.NE_RESIZE_CURSOR),
+            p2 -> new AxisRect(p2, Point.at(box.xmin(), box.ymax())));
+      case 13:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.SW_RESIZE_CURSOR),
+            p2 -> new AxisRect(p2, Point.at(box.xmax(), box.ymin())));
+      case 33:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.SE_RESIZE_CURSOR),
+            p2 -> new AxisRect(p2, Point.at(box.xmin(), box.ymin())));
+      case 21:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.N_RESIZE_CURSOR),
+            p2 -> new AxisRect(Point.at(box.xmin(), p2.y), Point.at(box.xmax(), box.ymax())));
+      case 23:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.S_RESIZE_CURSOR),
+            p2 -> new AxisRect(Point.at(box.xmin(), box.ymin()), Point.at(box.xmax(), p2.y)));
+      case 12:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.W_RESIZE_CURSOR),
+            p2 -> new AxisRect(Point.at(p2.x, box.ymin()), Point.at(box.xmax(), box.ymax())));
+      case 32:
+        return new DragAction(Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR),
+            p2 -> new AxisRect(Point.at(box.xmin(), box.ymin()), Point.at(p2.x, box.ymax())));
+      case 22:
+        return null;
+      default:
+        // fall through
+      }
+    }
+    return new DragAction(null, p2 -> new AxisRect(p1, p2));
+  }
+
+  private static final int GRABWIDTH = 3;
+
+  private static int placecode(double val, double min, double max) {
+    if( val < min - GRABWIDTH ) return 0;
+    if( val > max + GRABWIDTH ) return 4;
+    if( val > (min+max)/2 ) {
+      if( val > max - GRABWIDTH ) return 3;
+    } else {
+      if( val < min + GRABWIDTH ) return 1;
+    }
+    return 2;
   }
 
   public static BoxOverlay createBox(AxisRect lensBox) {
